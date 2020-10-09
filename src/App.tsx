9cbd2425979a9225cd1565224a5056e88c36f6cb
peeweetje <petravos70@gmail.com>
2020-10-09 16:44:23 +0200

    Feature: created Card component.

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,20 @@
-import React from 'react';
-import Button from './components/button/Button';
+import React from "react";
+import Button from "./components/button/Button";
+import Card from "./components/card/Card";
 
 function App() {
   return (
     <>
-      <Button color='violet'>Example</Button>
-      <Button color='gray'>Example</Button>
+      <Button color="violet">Example</Button>
+      <Button color="gray">Example</Button>
+      <Card>
+        <ul>
+       <h2>card-header</h2>
+       <li>
+       <p>This is an example of the card text</p>
+       </li>
+       </ul>
+      </Card>
     </>
   );
 }
