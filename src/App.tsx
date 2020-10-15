72ce8a3ee93b912474c1390457388e4a1b9a4adc
peeweetje <petravos70@gmail.com>
2020-10-15 22:11:57 +0200

    Feature: add font-family roboto to project. This is temporary, it will change.

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,28 @@
-import React from "react";
+import React from 'react';
 import { Switch, Route } from 'react-router';
 import Sidebar from './docs/components/sidebar/sidebar';
-import DocsButton from "./docs/components/button/docs-button";
-import DocsCard from "./docs/components/card/docs-card";
+import DocsButton from './docs/components/button/docs-button';
+import DocsCard from './docs/components/card/docs-card';
+import { styled } from './stitches.config';
 
+const AppContainer = styled('div', {
+  fontFamily: 'Roboto',
+});
 
-const App=() =>{
+const App = () => {
   return (
-    <div data-testid="app">
-     <Sidebar/>
-       <Switch>
-          <Route exact path="/button">
-            <DocsButton />
-          </Route>
-          <Route exact path="/card">
-            <DocsCard/>
-          </Route>
-        </Switch>
-     </div>
+    <AppContainer data-testid='app'>
+      <Sidebar />
+      <Switch>
+        <Route exact path='/button'>
+          <DocsButton />
+        </Route>
+        <Route exact path='/card'>
+          <DocsCard />
+        </Route>
+      </Switch>
+    </AppContainer>
   );
-}
+};
 
 export default App;
