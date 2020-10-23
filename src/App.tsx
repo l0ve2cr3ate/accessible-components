e7b2ecd2524505ec06d12fb7827ed8493806cea6
peeweetje <petravos70@gmail.com>
2020-10-23 13:43:13 +0200

    Refactor: change naming of files.

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
 import React from 'react';
 import { Switch, Route } from 'react-router';
-import Sidebar from './docs/components/sidebar/sidebar';
-import Example from './docs/components/example/example';
+import Sidebar from './docs/components/sidebar/Sidebar';
+import Example from './docs/components/example/Example';
 import Button from './components/button/Button';
 import Card from './components/card/Card';
 import { styled } from './stitches.config';
-import CardHeader from './components/card/card-header';
+import CardHeader from './components/card/CardHeader';
 
 const AppContainer = styled('div', {
   fontFamily: 'Roboto',
