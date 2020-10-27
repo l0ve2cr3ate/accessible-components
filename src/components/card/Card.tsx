e9fade65e309a9021af62e8b5670eeb29da87615
l0ve2cr3ate <l0ve2cr3ate@gmail.com>
2020-10-27 20:21:42 +0100

    fix: css typing in card component.

--- src/components/card/Card.tsx
+++ src/components/card/Card.tsx
@@ -1,21 +1,22 @@
-import React, { FC } from 'react';
-import { styled } from '../../stitches.config';
+import React, { FC } from "react";
+import { StitchesProps } from "@stitches/react";
+import { styled } from "../../stitches.config";
 
-const ContainerCard = styled('div', {
-  position: 'relative',
-  backgroundColor: '#fafafa',
-  color: '#333',
-  width: 'calc(35% - 1.25rem)',
-  padding: '0.625rem',
-  borderRadius: '0.625rem',
-  margin: '0.625rem',
-  height: '21.875rem',
-  boxShadow: 'inset 0 0.125rem 0.25rem 0 rgba(0, 0, 0, 0.06)',
+const ContainerCard = styled("div", {
+  position: "relative",
+  backgroundColor: "#fafafa",
+  color: "#333",
+  width: "calc(35% - 1.25rem)",
+  padding: "0.625rem",
+  borderRadius: "0.625rem",
+  margin: "0.625rem",
+  height: "21.875rem",
+  boxShadow: "inset 0 0.125rem 0.25rem 0 rgba(0, 0, 0, 0.06)",
 });
 
 export type CardProps = {
   children?: React.ReactNode;
-  css?: string;
+  css?: StitchesProps<typeof ContainerCard>["css"];
 };
 
 const Card: FC<CardProps> = ({ children, css }) => {
